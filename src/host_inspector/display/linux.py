4294884be8508096b6856fbd5c473c1bc6b534f9
Tim Santor <tsantor@xstudios.com>
2025-09-09 13:44:31 -0400

    update display tests

--- src/host_inspector/display/linux.py
+++ src/host_inspector/display/linux.py
@@ -4,8 +4,8 @@ def get_display_info() -> list[dict]:
         {
             "name": "Display",
             "display_id": 1,
-            "resolution_actual": {"width": 1920, "height": 1080},
-            "pixel_resolution": {"width": 1920, "height": 1080},
-            "refresh_hz": 60,
+            "resolution_actual": "1920 x 1080",
+            "pixel_resolution": "1920 x 1080",
+            "refresh_rate": "60 Hz",
         }
     ]
